23a2d5a7541ed20f197d96215a94e9a85ea1930b
ZorawarIQ200 <90725335+ZorawarIQ200@users.noreply.github.com>
2024-02-05 00:06:06 -0500

    Add files via upload

---
+++ src/Main/Main.java
@@ -0,0 +1,26 @@
+package Main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Game");
+
+		GamePanel GamePanel = new GamePanel();
+		window.add(GamePanel);
+
+		window.pack();
+
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		GamePanel.startGameThread();
+
+	}
+
+}
